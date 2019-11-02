d42392067765e2c5a3f0dbf96d163fe488a2103f
Fred Buchanan <oscillator.b@gmail.com>
2019-11-02 17:02:03 -0400

    Added reports list.

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,10 +1,26 @@
 <template>
   <v-layout
     column
-    justify-center
-    align-center
   >
-    <h1>Hello World</h1>
+    <h1>Reports</h1>
+    <v-card
+      class="mx-auto"
+      width="75%"
+      max-width="800"
+    >
+      <v-list-item
+        v-for="report in reports"
+        :key="report.id"
+        @click="go(report.id)"
+      >
+        <v-list-item-content>
+          <v-list-item-title>{{report.name}}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-avatar>
+          <v-icon>mdi-exit-to-app</v-icon>
+        </v-list-item-avatar>
+      </v-list-item>
+    </v-card>
     <nuxt-link to="/record">
       <v-btn
         color="blue"
@@ -22,7 +38,20 @@
 <script>
 
     export default {
-        components: {
+        async asyncData({$axios}) {
+            let reportsRequest = await $axios.get('/api/reports');
+            let reports = await reportsRequest.data;
+
+            return {
+                reports
+            }
+        },
+        methods: {
+            go(id) {
+              this.$router.push({
+                  path:`report/${id}`
+              })
+            }
         }
     }
 </script>
