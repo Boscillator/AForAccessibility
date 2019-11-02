2348ebc0e311a856780dee5594db81b22cea2ac9
MadSnailDisease <madsnaildisease123@gmail.com>
2019-11-02 19:13:21 -0400

    Removes celery in favor of multiprocessing

--- flask_app/app.py
+++ flask_app/app.py
@@ -11,39 +11,25 @@ from google.cloud import storage
 import string
 from celery import Celery
 import random
+from multiprocessing import Pool
 
 
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
 
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
+def upload_file(uid, filename, filedata, timestamp):
+    filename = secure_filename(filename)
+    filename = os.path.join(UPLOAD_FOLDER, filename)
+    with open(filename, "wb") as fp:
+        fp.write(base64.b64decode(filedata))
+    gcs_uri = get_uri_from_file(uid + ".wav", filename)
+    print(gcs_uri)
 
 
 app = Flask(__name__)
-
-app.config.update(
-    CELERY_IMPORTS = ("tasks", ),
-    CELERY_BROKER_URL='mongodb://localhost:27017/celery_db',
-    CELERY_RESULT_BACKEND='mongodb://localhost:27017/celery_db'
-)
-celery = make_celery(app)
-
 UPLOAD_FOLDER = "./uploads"
-
-import tasks
-
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+process_pool = Pool(4)
 
 
 def allowed_file(filename):
@@ -70,7 +56,7 @@ def api_receive_upload():
     if allowed_file(post_data["filename"]):
         filename = post_data["filename"] + ".wav"
         uid = make_uid()
-        tasks.upload_file.delay(uid, filename, post_data["filedata"], post_data["timestamp"])
+        process_pool.apply_async(upload_file, (uid, filename, post_data["filedata"], post_data["timestamp"]))
         return json.dumps({"status": "success", "message": "Uploaded file", "uid": uid})
 
     return json.dumps({"status": "failure", "message": "File name/type not permitted"}), 400
